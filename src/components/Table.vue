3349e63948d8deda31a5a266f3f3c5018c7056b2
jsalinas <jesus.salinas@smartfi.es>
2022-07-18 21:44:45 +0200

    volvemos al ruedo

--- src/components/Table.vue
+++ src/components/Table.vue
@@ -2,7 +2,7 @@
   <b-container fluid>
     <!-- User Interface controls -->
     <b-row>
-      <b-col lg="6" class="my-1">
+      <!-- <b-col lg="6" class="my-1">
         <b-form-group
           label="Sort"
           label-for="sort-by-select"
@@ -55,7 +55,7 @@
             size="sm"
           ></b-form-select>
         </b-form-group>
-      </b-col>
+      </b-col>-->
 
       <b-col lg="6" class="my-1">
         <b-form-group
@@ -75,16 +75,18 @@
             ></b-form-input>
 
             <b-input-group-append>
-              <b-button :disabled="!filter" @click="filter = ''">Clear</b-button>
+              <b-button :disabled="!filter" @click="filter = ''"
+                >Clear</b-button
+              >
             </b-input-group-append>
           </b-input-group>
         </b-form-group>
       </b-col>
 
-      <b-col lg="6" class="my-1">
+      <!--<b-col lg="6" class="my-1">
         <b-form-group
           v-model="sortDirection"
-          label="Filter On"
+          label="Filtro"
           description="Leave all unchecked to filter on all data"
           label-cols-sm="3"
           label-align-sm="right"
@@ -97,12 +99,15 @@
             :aria-describedby="ariaDescribedby"
             class="mt-1"
           >
-            <b-form-checkbox value="name">Name</b-form-checkbox>
-            <b-form-checkbox value="age">Age</b-form-checkbox>
-            <b-form-checkbox value="isActive">Active</b-form-checkbox>
+            <b-form-checkbox value="name">Nombre</b-form-checkbox>
+            <b-form-checkbox value="surname">Apellidos</b-form-checkbox>
+            <b-form-checkbox value="volunteer">Voluntario</b-form-checkbox>
+            <b-form-checkbox value="phone">Teléfono</b-form-checkbox>
+            <b-form-checkbox value="email">Email</b-form-checkbox>
+            <b-form-checkbox value="profession">Profesión</b-form-checkbox>
           </b-form-checkbox-group>
         </b-form-group>
-      </b-col>
+      </b-col> -->
 
       <b-col sm="5" md="6" class="my-1">
         <b-form-group
@@ -133,12 +138,12 @@
           size="sm"
           class="my-0"
         ></b-pagination>
-      </b-col>
+      </b-col> 
     </b-row>
 
     <!-- Main table element -->
     <b-table
-      :items="items"
+      :items="customers"
       :fields="fields"
       :current-page="currentPage"
       :per-page="perPage"
@@ -152,123 +157,150 @@
       small
       @filtered="onFiltered"
     >
-      <template #cell(name)="row">
-        {{ row.value.first }} {{ row.value.last }}
-      </template>
-
       <template #cell(actions)="row">
-        <b-button size="sm" @click="info(row.item, row.index, $event.target)" class="mr-1">
-          Info modal
+        <b-button
+          size="sm"
+          @click="info(customers[0], customers[0].length)"
+          class="mr-2"
+        >
+          Info
         </b-button>
-        <b-button size="sm" @click="row.toggleDetails">
-          {{ row.detailsShowing ? 'Hide' : 'Show' }} Details
+        <b-button size="sm" @click="row.toggleDetails" class="mr-2">
+          {{ row.detailsShowing ? "Ocultar" : "Mostrar" }} Detalles
         </b-button>
+        <b-button size="sm" variant="danger" v-b-modal.modal-lg>
+          Eliminar
+        </b-button>
+
+        <b-modal
+          id="modal-lg"
+          size="lg"
+          title="¡Atención!"
+          @click="deleteRow(customers[0].id)"
+          >¿Seguro que quiere eliminar este usuario?</b-modal
+        >
       </template>
 
       <template #row-details="row">
-        <b-card>
-          <ul>
-            <li v-for="(value, key) in row.item" :key="key">{{ key }}: {{ value }}</li>
-          </ul>
-        </b-card>
+        <b-row align-v="start" cols="6">
+          <b-card
+            v-for="(value, key) in row.item"
+            :key="key"
+            :header="key"
+            class="mr-3 mt-2 text-center"
+          >
+            <b-card-text>{{ value }}</b-card-text>
+          </b-card>
+        </b-row>
       </template>
     </b-table>
 
     <!-- Info modal -->
-    <b-modal :id="infoModal.id" :title="infoModal.title" ok-only @hide="resetInfoModal">
+    <b-modal
+      :id="infoModal.id"
+      :title="infoModal.title"
+      ok-only
+      @hide="resetInfoModal"
+    >
       <pre>{{ infoModal.content }}</pre>
     </b-modal>
   </b-container>
 </template>
 
 <script>
-  export default {
-    data() {
-      return {
-        items: [
-          { isActive: true, age: 40, name: { first: 'Dickerson', last: 'Macdonald' } },
-          { isActive: false, age: 21, name: { first: 'Larsen', last: 'Shaw' } },
-          {
-            isActive: false,
-            age: 9,
-            name: { first: 'Mini', last: 'Navarro' },
-            _rowVariant: 'success'
+import ApiService from "../common/api.service.js";
+export default {
+  data() {
+    return {
+      fields: [
+        {
+          key: "name",
+          label: "Nombre",
+          sortable: true,
+          sortDirection: "desc",
+        },
+        {
+          key: "surname",
+          label: "Apellidos",
+          sortable: true,
+          sortDirection: "desc",
+        },
+        {
+          key: "email",
+          label: "Email",
+          sortable: true,
+          class: "text-center",
+        },
+        {
+          key: "volunteer",
+          label: "Voluntario",
+          formatter: (value, key, item) => {
+            return value ? "Sí" : "No";
           },
-          { isActive: false, age: 89, name: { first: 'Geneva', last: 'Wilson' } },
-          { isActive: true, age: 38, name: { first: 'Jami', last: 'Carney' } },
-          { isActive: false, age: 27, name: { first: 'Essie', last: 'Dunlap' } },
-          { isActive: true, age: 40, name: { first: 'Thor', last: 'Macdonald' } },
-          {
-            isActive: true,
-            age: 87,
-            name: { first: 'Larsen', last: 'Shaw' },
-            _cellVariants: { age: 'danger', isActive: 'warning' }
-          },
-          { isActive: false, age: 26, name: { first: 'Mitzi', last: 'Navarro' } },
-          { isActive: false, age: 22, name: { first: 'Genevieve', last: 'Wilson' } },
-          { isActive: true, age: 38, name: { first: 'John', last: 'Carney' } },
-          { isActive: false, age: 29, name: { first: 'Dick', last: 'Dunlap' } }
-        ],
-        fields: [
-          { key: 'name', label: 'Person full name', sortable: true, sortDirection: 'desc' },
-          { key: 'age', label: 'Person age', sortable: true, class: 'text-center' },
-          {
-            key: 'isActive',
-            label: 'Is Active',
-            formatter: (value, key, item) => {
-              return value ? 'Yes' : 'No'
-            },
-            sortable: true,
-            sortByFormatted: true,
-            filterByFormatted: true
-          },
-          { key: 'actions', label: 'Actions' }
-        ],
-        totalRows: 1,
-        currentPage: 1,
-        perPage: 5,
-        pageOptions: [5, 10, 15, { value: 100, text: "Show a lot" }],
-        sortBy: '',
-        sortDesc: false,
-        sortDirection: 'asc',
-        filter: null,
-        filterOn: [],
-        infoModal: {
-          id: 'info-modal',
-          title: '',
-          content: ''
-        }
-      }
-    },
-    computed: {
-      sortOptions() {
-        // Create an options list from our fields
-        return this.fields
-          .filter(f => f.sortable)
-          .map(f => {
-            return { text: f.label, value: f.key }
-          })
-      }
-    },
-    mounted() {
-      // Set the initial number of items
-      this.totalRows = this.items.length
-    },
-    methods: {
-      info(item, index, button) {
-        this.infoModal.title = `Row index: ${index}`
-        this.infoModal.content = JSON.stringify(item, null, 2)
-        this.$root.$emit('bv::show::modal', this.infoModal.id, button)
+          sortable: true,
+          sortByFormatted: true,
+          filterByFormatted: true,
+        },
+        { key: "actions", label: "Actions" },
+      ],
+      totalRows: 1,
+      currentPage: 1,
+      perPage: 15,
+      pageOptions: [5, 10, 15, { value: 100, text: "Show a lot" }],
+      sortBy: "",
+      sortDesc: false,
+      sortDirection: "asc",
+      filter: null,
+      filterOn: [],
+      infoModal: {
+        id: "info-modal",
+        title: "",
+        content: "",
       },
-      resetInfoModal() {
-        this.infoModal.title = ''
-        this.infoModal.content = ''
-      },
-      onFiltered(filteredItems) {
-        // Trigger pagination to update the number of buttons/pages due to filtering
-        this.totalRows = filteredItems.length
-        this.currentPage = 1
-      }
-    }
-  }
+      customers: [],
+    };
+  },
+  computed: {
+    sortOptions() {
+      // Create an options list from our fields
+      return this.fields
+        .filter((f) => f.sortable)
+        .map((f) => {
+          return { text: f.label, value: f.key };
+        });
+    },
+  },
+  mounted() {
+    ApiService.get("customers").then((response) => {
+      this.customers = response.data;
+    });
+
+    // Set the initial number of customers
+    this.totalRows = this.customers.length;
+  },
+  methods: {
+    dataFormatTable() {
+      return this.customers[0]["email"].trim();
+    },
+    info(item, index, button) {
+      this.infoModal.title = `Row index: ${index}`;
+      this.infoModal.content = JSON.stringify(item, null, 2);
+      this.$root.$emit("bv::show::modal", this.infoModal.id, button);
+    },
+    resetInfoModal() {
+      this.infoModal.title = "";
+      this.infoModal.content = "";
+    },
+    onFiltered(filteredItems) {
+      // Trigger pagination to update the number of buttons/pages due to filtering
+      this.totalRows = filteredItems.length;
+      this.currentPage = 1;
+    },
+    deleteRow(id) {
+      ApiService.delete("customer/" + id).then((response) => {
+        console.log("ok");
+      });
+    },
+  },
+};
 </script>
